b0402687d1b4bccc5ec34bb78e2c384d502179af
Supun Subodha <supunmalsingha@gmail.com>
2026-07-31 19:41:33 +0530

    Added icons for dashboard cards

--- app/Components/Models/DashBoardMenuCard.cs
+++ app/Components/Models/DashBoardMenuCard.cs
@@ -3,11 +3,13 @@ public class DashBoardMenuCard
 	public string Title { get; set; }
 	public string Role { get; set; } //farmers,buyers,all
 	public string URL { get; set; }
+	public string Icon { get; set; }
 
-	public DashBoardMenuCard(string title, string role, string url)
+	public DashBoardMenuCard(string title, string role, string url, string icon)
 	{
 		Title = title;
 		Role = role;
 		URL = url;
+		Icon = icon;
 	}
 }
